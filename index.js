26c018f69978b4fe66700fbc65ad1c944236edbe
Anders Riutta <git@andersriutta.com>
2015-11-24 11:06:35 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var Rx = require('rx');
+var RxNode = require('rx-node');
+
+RxNode.fromUnpauseableStream = function(stream, finishEventName) {
+  if (stream.pause) {
+    stream.pause();
+  }
+
+  finishEventName = finishEventName || 'end';
+
+  return Rx.Observable.create(function(observer) {
+    function dataHandler(data) {
+      observer.onNext(data);
+    }
+
+    function errorHandler(err) {
+      observer.onError(err);
+    }
+
+    function endHandler() {
+      observer.onCompleted();
+    }
+
+    stream.addListener('data', dataHandler);
+    stream.addListener('error', errorHandler);
+    stream.addListener(finishEventName, endHandler);
+
+    if (stream.resume) {
+      stream.resume();
+    }
+
+    return function() {
+      stream.removeListener('data', dataHandler);
+      stream.removeListener('error', errorHandler);
+      stream.removeListener(finishEventName, endHandler);
+    };
+  }).publish().refCount();
+};
+
+module.exports = RxNode;
