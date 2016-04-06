9a562eed117c96ff62f75981bd334f075af51dfc
Anders Riutta <git@andersriutta.com>
2016-04-05 20:53:31 -0700

    Use typical index.js and lib/main.js structure.

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-var Rx = require('rx');
-
-// Note: the following is just included in the node build,
-// not the browser build
-var RxNode = require('rx-node');
-
-Rx.config.longStackSupport = true;
-
-require('./lib/from-unpausable-stream.js')(Rx);
-require('./lib/hierarchical-partition.js')(Rx);
-require('./lib/split-on-change.js')(Rx);
-require('./lib/then.js')(Rx);
-require('./lib/to-node-callback.js')(Rx);
-
-// this one must be after then (maybe maybe others?)
-require('./lib/pan-wrap.js')(Rx, RxNode);
-
-module.exports = Rx;
+module.exports = require('./lib/main.js');
