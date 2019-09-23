8c4c89a952dab951a9b46ac7b0b6ffa403e2f63b
Anders Riutta <git@andersriutta.com>
2019-09-23 15:20:43 -0700

    Enable Nix dev env

---
+++ shell.nix
@@ -0,0 +1,38 @@
+{ pkgs ? import <nixpkgs> {}, stdenv ? pkgs.stdenv }:
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.nodejs-10_x
+    pkgs.nodePackages.node2nix
+
+    # TODO: should this be specified both in here
+    # and in the pkg for my custom vim?
+    pkgs.nodePackages.prettier
+    pkgs.nodePackages.typescript
+
+    # Yarn ecosystem?
+    #pkgs.nodePackages.lerna
+    #pkgs.yarn
+    # what about yarn2nix?
+    #pkgs.yarn2nix
+
+    # node-gyp dependencies (node-gyp compiles C/C++ Addons)
+    #   see https://github.com/nodejs/node-gyp#on-unix
+    pkgs.python2
+  ] ++ (if stdenv.isDarwin then [
+    # more node-gyp dependencies
+    # XCode Command Line Tools
+    # TODO: do we need cctools?
+    #pkgs.darwin.cctools
+  ] else [
+    # more node-gyp dependencies
+    pkgs.gnumake
+
+    # gcc and binutils disagree on the version of a
+    # dependency, so we need to binutils-unwrapped.
+    pkgs.gcc # also provides cc
+    pkgs.binutils-unwrapped # provides ar
+  ]);
+  shellHook = ''
+    #export PATH="$HOME/Documents/safer-npm:$PATH"
+  '';
+}
